8e918316ae0e001c0564f94c410e0df22561c272
hondoros <achwaf@gmail.com>
2018-07-21 18:38:19 +0100

    Add project files.

---
+++ SweetKidsText/SweetKidsText/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SweetKidsText.Models;
+
+namespace SweetKidsText.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
